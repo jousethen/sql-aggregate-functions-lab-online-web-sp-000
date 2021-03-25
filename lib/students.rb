ac09593ae02c7b42417195c9605ef016cea8aebc
Jouse then <jthen93@gmail.com>
2021-03-25 03:26:18 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,9 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT gpa from students order by gpa limit 1"
 end
 
 def average_student_gpa
+  
 end
 
 def total_tardies_for_all_students
